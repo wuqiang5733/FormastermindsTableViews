3441bacb7947183c4c490b8ed00decf6878858b3
xiaoqianghou <2662651103@qq.com>
2017-03-04 10:51:28 +0800

    Initial Commit

---
+++ FormastermindsTableViews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FormastermindsTableViews
+//
+//  Created by WuQiang on 2017/3/4.
+//  Copyright © 2017年 WuQiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
